27d7ab6ad4f577be99d2d01f036d711e5a4dad56
Tim <eaatie@students.eaaa.dk>
2021-02-27 12:33:35 +0100

    empty

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from random import seed
+from random import random
+import time
 
 app = FastAPI()
 
@@ -13,3 +15,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
 	return {"item_id": item_id, "q": q}
+
+
+@app.get("/dummytal/get")
+def give_dummy_data():
+	print(time.time())
+	seed(1)
+	print(random())
+	return {str(time.time()): str(random())}
