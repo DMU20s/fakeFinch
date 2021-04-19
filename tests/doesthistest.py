232398beb4f0cbb9fdceeb93f111a89b1158e77e
Tim <tim@veticus.com>
2021-04-19 20:19:05 +0200

    add travis yml

---
+++ tests/doesthistest.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+	def test_something(self):
+		assert sum([1, 2, 3]) == 6, "Should be 6"
+
+	def test_something_else(self):
+		self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+	unittest.main()
